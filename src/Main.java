9627530f1b2dcbd1dcaa37976d1bf4e0921de129
MrGrzesiek <78262537+MrGrzesiek@users.noreply.github.com>
2023-12-10 18:34:00 +0100

    test1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("ZPI2023_zaoczni_Ananaski WPruska");
         System.out.println("JanuszWdowka");
         System.out.println("WPruska");
+        //TEST1
         System.out.println("MrGrzesiek");
         System.out.println("vAdamski");
         ArithmeticsAdd add = new ArithmeticsAdd();
