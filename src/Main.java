98ff9dc4dc02de498b524cc18beb9b41868dc7d8
WPruska <236438@edu.p.lodz.pl>
2023-12-10 17:44:00 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ZPI2023_zaoczni_Ananaski WPruska");
         System.out.println("JanuszWdowka");
+        System.out.println("WPruska");
     }
 }
