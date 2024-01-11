7160ea919ab33c84fa45bf1a7e44ffca657d216b
MrGrzesiek <78262537+MrGrzesiek@users.noreply.github.com>
2024-01-11 11:53:43 +0100

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -5,7 +5,7 @@ public class SimpleTest {
 
     @Test
     public void testAddition() {
-        int result = 2 + 2;
+        int result = 2 + 3;
         // Sprawdź, czy wynik dodawania dwóch liczb jest równy 5
         assertEquals(5, result, "Expected result should be 5");
     }
